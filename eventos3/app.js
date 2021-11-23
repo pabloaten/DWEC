26d2a56d7a10b3cd682f4f7cf3ba6330721fd052
pabloaten <pabloatenciano@gmail.com>
2021-11-23 16:59:01 +0100

    a

---
+++ eventos3/app.js
@@ -0,0 +1,13 @@
+/**
+ * @description Flujo de eventos
+ */
+function info(evento){
+    //nombre del evento
+    console.log("Hola soy el evento "+ this.className+ "y el click lo lanzo el "+evento.target.className);
+
+}
+const eventosDiv =document.querySelectorAll(".flujos-eventos div");
+/* console.log(eventosDiv); */
+eventosDiv.forEach((div) =>{
+    div.addEventListener("click",info);
+})
