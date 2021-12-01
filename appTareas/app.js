cf51f64eea5bb6db7ff8785468bff5aeda1e899a
pabloaten <pabloatenciano@gmail.com>
2021-12-01 23:45:10 +0100

    Cambio

--- appTareas/app.js
+++ appTareas/app.js
@@ -1,14 +1,26 @@
+/**
+ * @author Pablo Atenciano Jurado
+ * @description Gestor de tareas con javascript
+ */
+
 let boton = document.getElementById("enviar");
 let titulo = document.getElementById("titulo");
 let miArray = [];
 let tareasEliminadas = [];
 let contador = 0;
 
+/**
+ * Funcion para actualizar el contador de tareas
+ */
 function actualizarContador() {
   let cont = (document.getElementById("contador").innerHTML = contador);
 }
 boton.addEventListener("click", añadirTarea);
 
+/**
+ * Funcion para obtener un string con la fecha
+ * @returns Un string con el formato de la fecha que necesitamos
+ */
 function obtenerFecha() {
   let fecha = new Date();
   let string =
@@ -23,7 +35,9 @@ function obtenerFecha() {
     fecha.getSeconds();
   return string;
 }
-
+/**
+ * Funcion para añadir la tarea
+ */
 function añadirTarea() {
   let tarea = document.getElementById("nombreTarea");
   let misTareas = document.getElementById("mistareas");
@@ -35,7 +49,7 @@ function añadirTarea() {
     let h1 = document.createElement("h1");
     h1.addEventListener("click", eliminar);
     let fecha = obtenerFecha();
-    let textoh1 = document.createTextNode(tarea.value+" "+fecha);
+    let textoh1 = document.createTextNode(tarea.value + " " + fecha);
     h1.appendChild(textoh1);
     misTareas.appendChild(h1);
     miArray.push(tarea.value);
@@ -45,10 +59,17 @@ function añadirTarea() {
     alert("Ya hay una tarea así creada");
   }
 }
+/**
+ * Funcion para poner el input vacio
+ */
 function setTexto() {
   let tarea = document.getElementById("nombreTarea");
   tarea.value = "";
 }
+/**
+ * Funcion para eliminar la tarea
+ * @param {e} Object
+ */
 function eliminar(e) {
   let h = e.target;
   let misTareas = document.getElementById("mistareas");
@@ -56,48 +77,48 @@ function eliminar(e) {
   contador--;
   actualizarContador();
   for (let a of miArray) {
-    console.log(h.innerText);
-    let long =a.length;
-    let valor =  h.innerText;
-    let h1=  valor.substr(0,long);
-    console.log(h1);
+    let long = a.length;
+    let valor = h.innerText;
+    let h1 = valor.substr(0, long);
     if (a === h1) {
-      tareasEliminadas.push(a);
+      tareasEliminadas.push(h1);
       let num = miArray.indexOf(a);
       miArray.splice(num, 1);
+      console.log("Tarea eliminada " + h.innerText);
+      console.log("Todas las tareas eliminadas " + tareasEliminadas);
     }
   }
 }
+/**
+ * Funcion para presionar el boton enter y ejecutar la funcion de añadir tarea
+ */
 document.addEventListener("keypress", (e) => {
   if (e.code === "Enter") {
     añadirTarea();
   }
 });
-
+/**
+ * Funcion realizar la busqueda
+ */
 let buscador = document.getElementById("buscador");
 buscador.addEventListener("keypress", (e) => {
   let tareaEncontrada = document.getElementById("tareaEncontrada");
-  console.log(buscador.value);
+
   if (buscador.value === "") {
   } else {
-    for (const a of miArray) {
+    for (let a of miArray) {
       if (a.toString().includes(e.target.value)) {
         let h2 = document.createElement("h1");
         let texto = document.createTextNode(a.toString());
         h2.appendChild(texto);
         tareaEncontrada.appendChild(h2);
-        console.log(miArray.length);
-        break;
       }
     }
   }
+
   let buscar = document.getElementById("buscar");
   buscar.addEventListener("click", (e) => {
     let tareaEncontrada = document.getElementById("tareaEncontrada");
-    tareaEncontrada.children[0].remove();
+    tareaEncontrada.remove(2);
   });
-  console.log(a.toString().includes(e.target.value));
-  console.log(e.target.value);
 });
-
-console.log(tareasEliminadas);
